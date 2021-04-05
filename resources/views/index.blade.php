814149de7a7d31d313964bb871f26a0c1b51d076
Sandeep Singh <serperior2@gmail.com>
2021-04-05 11:38:46 -0400

    set storage and  public-storage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h1>Hewwo!</h1>
+    <img src="{{ asset('storage/skills/ComeBack.png') }}" alt="">
+    <span>{{ Storage::disk('local')->get('splatdata/GearInfo_Head.json') }}</span>
 @endsection

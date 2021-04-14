097bdbc5b27505481a636e27f6d9c303c2c47444
Sandeep Singh <serperior2@gmail.com>
2021-04-14 14:02:26 -0400

    fix gear-piece view

--- resources/views/users/gear-pieces/index.blade.php
+++ resources/views/users/gear-pieces/index.blade.php
@@ -1,72 +1,78 @@
-All gear-pieces
+@extends('layouts.app')
 
-@if ($gearpieces->count())
-    @foreach ($gearpieces as $gearpiece)
+@section('content')
+    
+    <h1>{{ $user->username }}'s gear-pieces</h1>
 
-        {{-- get skill names and push into array --}}
-        @if ($gearpiece->gear_piece_main !== null)
-            @foreach ($skills as $skill)
-                @if ($skill['id'] == $gearpiece->gear_piece_main)
-                    @php
-                        $skillNames[] = $skill['skill'];
-                    @endphp
-                @endif
-            @endforeach
-        @else
-            @php
-                $skillNames[] = 'unknown';
-            @endphp
-        @endif
+    @if ($gearpieces->count())
+        @foreach ($gearpieces as $gearpiece)
 
-        @if ($gearpiece->gear_piece_sub_1 !== null)
-            @foreach ($skills as $skill)
-                @if ($skill['id'] == $gearpiece->gear_piece_sub_1)
-                    @php
-                        $skillNames[] = $skill['skill'];
-                    @endphp
-                @endif
-            @endforeach
-        @else
-            @php
-                $skillNames[] = 'unknown';
-            @endphp
-        @endif
+            {{-- get skill names and push into array --}}
+            @if ($gearpiece->gear_piece_main !== null)
+                @foreach ($skills as $skill)
+                    @if ($skill['id'] == $gearpiece->gear_piece_main)
+                        @php
+                            $skillNames[] = $skill['skill'];
+                        @endphp
+                    @endif
+                @endforeach
+            @else
+                @php
+                    $skillNames[] = 'unknown';
+                @endphp
+            @endif
 
-        @if ($gearpiece->gear_piece_sub_2 !== null)
-            @foreach ($skills as $skill)
-                @if ($skill['id'] == $gearpiece->gear_piece_sub_2)
-                    @php
-                        $skillNames[] = $skill['skill'];
-                    @endphp
-                @endif
-            @endforeach
-        @else
-            @php
-                $skillNames[] = 'unknown';
-            @endphp
-        @endif
+            @if ($gearpiece->gear_piece_sub_1 !== null)
+                @foreach ($skills as $skill)
+                    @if ($skill['id'] == $gearpiece->gear_piece_sub_1)
+                        @php
+                            $skillNames[] = $skill['skill'];
+                        @endphp
+                    @endif
+                @endforeach
+            @else
+                @php
+                    $skillNames[] = 'unknown';
+                @endphp
+            @endif
 
-        @if ($gearpiece->gear_piece_sub_3 !== null)
-            @foreach ($skills as $skill)
-                @if ($skill['id'] == $gearpiece->gear_piece_sub_3)
-                    @php
-                        $skillNames[] = $skill['skill'];
-                    @endphp
-                @endif
-            @endforeach
-        @else
-            @php
-                $skillNames[] = 'unknown';
-            @endphp
-        @endif
+            @if ($gearpiece->gear_piece_sub_2 !== null)
+                @foreach ($skills as $skill)
+                    @if ($skill['id'] == $gearpiece->gear_piece_sub_2)
+                        @php
+                            $skillNames[] = $skill['skill'];
+                        @endphp
+                    @endif
+                @endforeach
+            @else
+                @php
+                    $skillNames[] = 'unknown';
+                @endphp
+            @endif
+
+            @if ($gearpiece->gear_piece_sub_3 !== null)
+                @foreach ($skills as $skill)
+                    @if ($skill['id'] == $gearpiece->gear_piece_sub_3)
+                        @php
+                            $skillNames[] = $skill['skill'];
+                        @endphp
+                    @endif
+                @endforeach
+            @else
+                @php
+                    $skillNames[] = 'unknown';
+                @endphp
+            @endif
 
 
 
 
-        {{-- gearpiece component --}}
-        <x-gear-piece.base :gearpiece="$gearpiece" :skills="$skillNames" />
-        
-    @endforeach
-@else
-    {{ $user }} does not have any gear pieces yet...
-@endif
+            {{-- gearpiece component --}}
+            <x-gear-piece.base :gearpiece="$gearpiece" :skills="$skillNames" />
+            
+        @endforeach
+    @else
+        {{ $user }} does not have any gear pieces yet...
+    @endif
+
+@endsection

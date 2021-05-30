6acebe9593cbaae9818d477897b54e67eaa68fe2
Sandeep Singh <serperior2@gmail.com>
2021-05-30 19:37:34 -0400

    styling for gear edit

--- resources/views/users/gears/edit.blade.php
+++ resources/views/users/gears/edit.blade.php
@@ -14,24 +14,28 @@
     @endforeach
 
     {{-- form --}}
-    <form action="{{ route('gears.update', [$user, $gear]) }}" method="post">
+    <form action="{{ route('gears.update', [$user, $gear]) }}" method="post" class="w-full md:w-1/2 lg:w-1/3 px-4 md:px-0 md:mx-auto">
         @method('PUT')
         @csrf
 
+        <div class="mb-8">
+            <h2 class="text-2xl font-bold text-center">Edit Gear</h2>
+        </div>
+
         {{-- gear name --}}
-        <div>
+        <div class="mb-4">
             <label for="gear-name" class="block">Gear name:</label>
-            <input type="text" name="gear-name" id="gear-name" value="{{ $gear->gear_name }}">
+            <input type="text" name="gear-name" id="gear-name" value="{{ $gear->gear_name }}" class="w-full rounded focus:ring-primary-400 focus:border-primary-400">
         </div>
 
         {{-- gear desc --}}
-        <div>
+        <div class="mb-4">
             <label for="gear-desc" class="block">Gear description:</label>
-            <textarea name="gear-desc" id="gear-desc" cols="30" rows="3">{{ $gear->gear_desc }}</textarea>
+            <textarea name="gear-desc" id="gear-desc" cols="30" rows="3" class="w-full rounded focus:ring-primary-400 focus:border-primary-400">{{ $gear->gear_desc }}</textarea>
         </div>
 
         {{-- gear and skills selector --}}
-        <div class="w-1/3 mx-auto bg-indigo-500">
+        <div class="mb-6">
             <x-gear.gear-skills-builder 
                 :gears="$gears" 
                 :skills="$skillsData" 
@@ -45,7 +49,7 @@
         </div>
 
         {{-- submit --}}
-        <input type="submit" class="p-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 hover:cursor-pointer" value="Update">
+        <input type="submit" class="p-2 bg-transparent text-primary-700 rounded-md border border-primary-700 mb-2 cursor-pointer transition-colors hover:bg-primary-500 hover:text-white hover:border-primary-500" value="Update">
     </form>
 @endsection
 

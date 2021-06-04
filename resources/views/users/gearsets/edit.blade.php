1a2b6ff85120044f640599578ee7860174831256
Sandeep Singh <serperior2@gmail.com>
2021-06-03 23:26:21 -0400

    styling for gearset Edit

--- resources/views/users/gearsets/edit.blade.php
+++ resources/views/users/gearsets/edit.blade.php
@@ -9,59 +9,71 @@
 @endsection
 
 @section('content')
-    <div class="w-1/2 mx-auto">
-        <form action="{{ route('gearsets.update', [$user, $gearset]) }}" method="post">
-            @method('PUT')
-            @csrf
+    <form action="{{ route('gearsets.update', [$user, $gearset]) }}" method="post" class="w-full md:w-3/4 lg:w-2/3 px-4 md:px-0 md:mx-auto">
+        @method('PUT')
+        @csrf
 
+        <div class="mb-8">
+            <h2 class="text-2xl font-bold text-center">Edit Gearset</h2>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 mb-4">
             {{-- gearset name --}}
-            <div>
+            <div class="mb-4">
                 <label for="gearset-name" class="block">Gearset name:</label>
-                <input type="text" name="gearset-name" id="gearset-name" value="{{ $gearset->gearset_name }}">
+                <input type="text" name="gearset-name" id="gearset-name" value="{{ $gearset->gearset_name }}" class="w-full rounded focus:ring-primary-400 focus:border-primary-400">
             </div>
 
             {{-- gearset desc --}}
-            <div>
+            <div class="row-start-2 mb-4 md:mb-0">
                 <label for="gearset-desc" class="block">Gearset description:</label>
-                <textarea name="gearset-desc" id="gearset-desc">{{ $gearset->gearset_desc }}</textarea>
+                <textarea name="gearset-desc" id="gearset-desc" class="w-full rounded focus:ring-primary-400 focus:border-primary-400">{{ $gearset->gearset_desc }}</textarea>
             </div>
 
             {{-- gearset mode --}}
-            <div>
+            <div class="row-span-2">
                 <label for="mode" class="block">Game mode:</label>
 
-                <input type="checkbox" name="gearset-mode-rm" value="1" @if($gearset->gearset_mode_rm) checked @endif>
-                <label for="gearset-mode-rm">Rainmaker</label>
+                <div>
+                    <input type="checkbox" name="gearset-mode-rm" value="1" @if($gearset->gearset_mode_rm) checked @endif class="rounded-sm text-secondary-700 focus:ring-primary-400">
+                    <label for="gearset-mode-rm">Rainmaker</label>
+                </div>
 
-                <input type="checkbox" name="gearset-mode-cb" value="1" @if($gearset->gearset_mode_cb) checked @endif>
-                <label for="gearset-mode-cb">Clam Blitz</label>
+                <div>
+                    <input type="checkbox" name="gearset-mode-cb" value="1" @if($gearset->gearset_mode_cb) checked @endif class="rounded-sm text-secondary-700 focus:ring-primary-400">
+                    <label for="gearset-mode-cb">Clam Blitz</label>
+                </div>
 
-                <input type="checkbox" name="gearset-mode-sz" value="1" @if($gearset->gearset_mode_sz) checked @endif>
-                <label for="gearset-mode-sz">Splat Zones</label>
+                <div>
+                    <input type="checkbox" name="gearset-mode-sz" value="1" @if($gearset->gearset_mode_sz) checked @endif class="rounded-sm text-secondary-700 focus:ring-primary-400">
+                    <label for="gearset-mode-sz">Splat Zones</label>
+                </div>
 
-                <input type="checkbox" name="gearset-mode-tc" value="1" @if($gearset->gearset_mode_tc) checked @endif>
-                <label for="gearset-mode-tc">Tower Control</label>
+                <div>
+                    <input type="checkbox" name="gearset-mode-tc" value="1" @if($gearset->gearset_mode_tc) checked @endif class="rounded-sm text-secondary-700 focus:ring-primary-400">
+                    <label for="gearset-mode-tc">Tower Control</label>
+                </div>
             </div>
+        </div>
 
-            <div class="grid grid-cols-2 grid-rows-2 gap-4 min-w-min w-full max-w-max">
-                {{-- weapons --}}
-                <livewire:weapon :weapons="$splatdata[4]" :oldWeapon="$gearset->gearset_weapon_id" />
-        
-                {{-- gear (head) --}}
-                <livewire:gear :gears="$gears" gearType="head" :skills="$splatdata[3]" :oldGears="$currentGears" />
-        
-                {{-- gear (clothes) --}}
-                <livewire:gear :gears="$gears" gearType="clothes" :skills="$splatdata[3]" :oldGears="$currentGears" />
-                
-                {{-- gear (shoes) --}}
-                <livewire:gear :gears="$gears" gearType="shoes" :skills="$splatdata[3]" :oldGears="$currentGears" />
-            </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+            {{-- weapons --}}
+            <livewire:weapon :weapons="$splatdata[4]" :oldWeapon="$gearset->gearset_weapon_id" />
+    
+            {{-- gear (head) --}}
+            <livewire:gear :gears="$gears" gearType="head" :skills="$splatdata[3]" :oldGears="$currentGears" />
+    
+            {{-- gear (clothes) --}}
+            <livewire:gear :gears="$gears" gearType="clothes" :skills="$splatdata[3]" :oldGears="$currentGears" />
+            
+            {{-- gear (shoes) --}}
+            <livewire:gear :gears="$gears" gearType="shoes" :skills="$splatdata[3]" :oldGears="$currentGears" />
+        </div>
 
 
 
-            <input type="submit" value="Update" class="p-2 border-black rounded-md bg-indigo-200 border hover:bg-indigo-400 mt-4">
-        </form>
-    </div>
+        <input type="submit" value="Update" class="p-2 bg-transparent text-primary-700 rounded-md border border-primary-700 mb-2 cursor-pointer transition-colors hover:bg-primary-500 hover:text-white hover:border-primary-500">
+    </form>
 @endsection
 
 @section('scripts')

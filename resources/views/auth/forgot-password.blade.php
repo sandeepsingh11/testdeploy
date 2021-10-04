d911951156a8c054b0a6c34f3b1fec8d87021f81
Sandeep Singh <serperior2@gmail.com>
2021-10-03 21:13:50 -0400

    style fix

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,17 +3,20 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded-lg">
+            
+            @if (session('status'))
+                {{-- success message --}}
+                <div class="bg-green-500 p-4 rounded-lg mb-6 text-white text-center">
+                    {{ session('status') }}
+                </div>
+            @endif
+            
             <form action="{{ route('password.email') }}" method="post">
                 @csrf
 
                 <div class="mb-4">
                     <label for="email" class="sr-only">email</label>
                     <input type="email" name="email" id="email" placeholder="email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}" required autofocus>
-                    
-                    {{-- success message --}}
-                    <div class="text-green-500 text-center mt-1">
-                        {{ session('status') }}
-                    </div>
 
                     @error('email')
                         <div class="text-red-500 mt-2 text-sm">

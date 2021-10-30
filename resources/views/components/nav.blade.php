c42292cc147b0b0bf364d708da0ed682276bf079
Sandeep Singh <serperior2@gmail.com>
2021-10-30 14:37:00 -0400

    add mobile nav menu

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,7 +3,8 @@
         <x-nav-link link="{{ route('home') }}" text="SB" />
     </ul>
 
-    <ul class="flex items-center">
+    {{-- desktop menu --}}
+    <ul class="md:flex items-center hidden">
         @auth
             <x-nav-link link="{{ route('gears.create', Request::user()) }}" text="+Gear" />
             <x-nav-link link="{{ route('gearsets.create', Request::user()) }}" text="+Gearset" />
@@ -26,14 +27,14 @@
                     class="absolute top-full right-0 bg-primary-500 mt-1 rounded text-left z-10"
                     style="display: none"
                 >
-                    <x-nav-link link="{{ route('dashboard', Request::user()) }}" text="Dashboard" class="py-2 px-4" />
+                    <x-nav-link link="{{ route('dashboard', Request::user()) }}" text="Dashboard" class="py-2 px-4 rounded-t-md" />
                     <x-nav-link link="{{ route('gears', Request::user()) }}" text="Gears" class="py-2 px-4" />
                     <x-nav-link link="{{ route('gearsets', Request::user()) }}" text="Gearsets" class="py-2 px-4" />
                     {{-- <x-nav-link link="{{ route('settings', Request::user()) }}" text="Settings" class="py-2 px-4" /> --}}
                     <li>
                         <form action="{{ route('logout') }}" method="post">
                             @csrf
-                            <button type="submit" class="w-full py-2 px-4 text-left hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors">Logout</button>
+                            <button type="submit" class="w-full py-2 px-4 text-left rounded-b-md hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors">Logout</button>
                         </form>
                     </li>
                 </ul>
@@ -44,5 +45,57 @@
             <x-nav-link link="{{ route('login') }}" text="Login" />
             <x-nav-link link="{{ route('register') }}" text="Register" />
         @endguest
+    </ul>
+
+
+
+    {{-- mobile menu --}}
+    <ul class="md:hidden items-center flex">
+        {{-- dropdown container --}}
+        <div x-data="{ open: false }" class="relative">
+            <button 
+                @click="open = true" 
+                class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors"
+            >
+                <div x-show="!open">
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </div>
+                <div x-show="open">
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </div>
+            </button>
+
+            {{-- dropdown --}}
+            <ul
+                x-show.transition="open"
+                @click.away="open = false"
+                class="fixed left-0 flex flex-col justify-evenly w-screen bg-primary-500 text-center"
+                style="display: none"
+            >
+                @auth
+                    <x-nav-link link="{{ route('gears.create', Request::user()) }}" text="+Gear" />
+                    <x-nav-link link="{{ route('gearsets.create', Request::user()) }}" text="+Gearset" />
+                    <x-nav-link link="{{ route('dashboard', Request::user()) }}" text="Dashboard" />
+                    <x-nav-link link="{{ route('gears', Request::user()) }}" text="Gears" />
+                    <x-nav-link link="{{ route('gearsets', Request::user()) }}" text="Gearsets" />
+                    {{-- <x-nav-link link="{{ route('settings', Request::user()) }}" text="Settings" /> --}}
+                    <li>
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button type="submit" class="w-full p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors">Logout</button>
+                        </form>
+                    </li>
+                @endauth
+
+                @guest
+                    <x-nav-link link="{{ route('login') }}" text="Login" />
+                    <x-nav-link link="{{ route('register') }}" text="Register" />
+                @endguest
+            </ul>
+        </div>
     </ul>
 </nav>

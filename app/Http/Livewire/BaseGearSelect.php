7ef70447323de61c686cd3d74d108b644a80c49d
Sandeep Singh <serperior2@gmail.com>
2021-12-05 17:30:10 -0500

    Revert "update gear selection on search"
    This reverts commit 65c3c1933c472a86ef332ea7e4caadb98e9568ef.

--- app/Http/Livewire/BaseGearSelect.php
+++ app/Http/Livewire/BaseGearSelect.php
@@ -59,13 +59,5 @@ class BaseGearSelect extends Component
         else {
             $this->filteredList = $this->searchable;
         }
-
-        // if empty list, fill with default
-        if (!isset(array_keys($this->filteredList)[0])) {
-            $this->filteredList = $this->searchable;
-        }
-
-        // update gear selection
-        $this->selectUpdate(array_keys($this->filteredList)[0]); 
     }
 }
